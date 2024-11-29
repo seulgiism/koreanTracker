cf905bc92f5a31c81e4e1beeec09dbeab7e14a17
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-11-29 19:53:03 +0100

    add main.c, dynamic flag saving

---
+++ main.c
@@ -0,0 +1,138 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// will store the positions of add flags into the vars
+// 0 means it was not found
+void argv_set_flags_add(char **argv, short argc, char** name, char** link, short* watched);
+void argv_set_flags_rm(char **argv, short argc, short* id);
+void argv_set_flags_show(char **argv, short argc, short* id);
+void argv_set_flags_increment(char **argv, short argc, short* id);
+
+
+
+
+////////* MAIN *////////
+
+int main(int argc, char** argv) {
+    argc = (short)argc;
+
+    // entrance to add, remove, show, increment, help
+    if (argc > 1) {
+
+        // ADD feature
+        if      (strcmp(argv[1], "add") == 0) {
+
+            // store flags
+            char* name = NULL;
+            char* link = NULL;
+            short watched = 0;
+            argv_set_flags_add(argv, argc, &name, &link, &watched);
+
+            // 
+
+            return 0;
+        }
+
+        // REMOVE feature
+        else if (strcmp(argv[1], "rm") == 0) {
+            
+            // store flags
+            short id = 0;
+            argv_set_flags_rm(argv, argc, &id);
+
+            
+
+            return 0;
+        }
+
+        // SHOW feature
+        else if (strcmp(argv[1], "show") == 0) {
+            
+            // store flags
+            short id = 0;
+            argv_set_flags_show(argv, argc, &id);
+
+            
+
+            return 0;
+        }
+
+        // INCREMENT feature
+        else if (strcmp(argv[1], "increment") == 0) {
+
+            // store flags
+            short id = 0;
+            argv_set_flags_increment(argv, argc, &id);
+
+            
+
+            return 0;
+        }
+
+        else {
+
+            // HELP feature
+            if (strcmp(argv[1], "-h") || strcmp(argv[1], "--help")) {
+                
+            }
+
+            return 0;
+        }
+    }
+
+    // error occured
+
+    printf("ERROR");
+
+    return 1;
+}
+
+
+
+
+////* command line flag position storer *////
+
+
+// will store the positions of add flags into the vars
+// 0 means it was not found
+void argv_set_flags_add(char **argv, short argc, char** name, char** link, short* watched) {
+    for (short i = 2; i < argc; i++) {
+
+        // set flags
+        if (((strcmp(argv[i], "--name")    == 0) || (strcmp(argv[i], "-n") == 0)) && (i + 1) < argc) *name    = argv[i+1];
+        if (((strcmp(argv[i], "--link")    == 0) || (strcmp(argv[i], "-l") == 0)) && (i + 1) < argc) *link    = argv[i+1];
+        if (((strcmp(argv[i], "--watched") == 0) || (strcmp(argv[i], "-w") == 0)) && (i + 1) < argc) *watched = (short)(atoi(argv[i+1]));
+    }
+}
+
+// will store the positions of add flags into the vars
+// 0 means it was not found
+void argv_set_flags_rm(char **argv, short argc, short* id) {
+    for (short i = 2; i < argc; i++) {
+
+        // set flags
+        if (((strcmp(argv[i], "--id") == 0) || (strcmp(argv[i], "-i") == 0)) && (i + 1) < argc) *id = (short)(atoi(argv[i+1]));
+    }
+}
+
+// will store the positions of add flags into the vars
+// 0 means it was not found
+void argv_set_flags_show(char **argv, short argc, short* id) {
+    for (short i = 2; i < argc; i++) {
+
+        // set flags
+        if (((strcmp(argv[i], "--id") == 0) || (strcmp(argv[i], "-i") == 0)) && (i + 1) < argc) *id = (short)(atoi(argv[i+1]));
+    }
+}
+
+// will store the positions of add flags into the vars
+// 0 means it was not found
+void argv_set_flags_increment(char **argv, short argc, short* id) {
+    for (short i = 2; i < argc; i++) {
+
+        // set flags
+        if (((strcmp(argv[i], "--id") == 0) || (strcmp(argv[i], "-i") == 0)) && (i + 1) < argc) *id = (short)(atoi(argv[i+1]));
+    }
+}
