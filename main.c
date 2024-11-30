3e7c6ba77d0abaa9e1970e7d6f16c15fa1fc2efe
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-11-30 01:01:43 +0100

    add (main.c), katarray_static_init() for initializing katarray

--- main.c
+++ main.c
@@ -2,6 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define KATARRAY_CAPACITY 50
+
+typedef struct KoreanData{
+    short watches;
+    char* name;
+    char* link;
+} korean_data_t;
+
+
+typedef struct KatArray{
+    size_t length;
+    size_t capacity;
+
+    size_t index_start;
+    size_t index_end;
+
+    char shrinkable;
+    void* initial_value;
+    void* list[KATARRAY_CAPACITY];
+} katarray_voidp_t;
+
 
 // will store the positions of add flags into the vars
 // 0 means it was not found
@@ -10,6 +31,8 @@ void argv_set_flags_rm(char **argv, short argc, short* id);
 void argv_set_flags_show(char **argv, short argc, short* id);
 void argv_set_flags_increment(char **argv, short argc, short* id);
 
+// initialize katarray
+katarray_voidp_t* katarray_static_init();
 
 
 
@@ -135,4 +158,23 @@ void argv_set_flags_increment(char **argv, short argc, short* id) {
         // set flags
         if (((strcmp(argv[i], "--id") == 0) || (strcmp(argv[i], "-i") == 0)) && (i + 1) < argc) *id = (short)(atoi(argv[i+1]));
     }
+}
+
+
+
+////* katarray initializer *////
+katarray_voidp_t* katarray_static_init() {
+    // initialize katarray_voidp_t
+    katarray_voidp_t *KatArray;
+
+    KatArray->length        = 0;
+    KatArray->capacity      = KATARRAY_CAPACITY;
+
+    KatArray->index_start   = __SIZE_MAX__;
+    KatArray->index_end     = __SIZE_MAX__;
+
+    KatArray->initial_value = NULL;
+    KatArray->shrinkable    = -1;
+
+    return KatArray;
 }
